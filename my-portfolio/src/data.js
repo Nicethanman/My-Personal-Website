63d5a94588814021d3708dff149685a506758820
Nicholas Chang <143849035+Nicethanman@users.noreply.github.com>
2024-01-10 12:56:41 -0800

    First Commit

---
+++ my-portfolio/src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Hello Command",
+    subtitle: "Firmware Development in C",
+    description:
+      "I Implemented a command for UBC Orbit's ALEASAT satellite that output a Hello message. The command was tested the code by manually flashing to a Hercules microcontroller and writing a hardware in the loop test in Python",
+    image: "./project-1.gif",
+    link: "https://github.com/Nicethanman/Hello-Command",
+  },
+  {
+    title: "RISC Machine",
+    subtitle: "System Verilog",
+    description:
+      "In the class Computing Systems I, I created a Turing Complete reduced instruction set machine (RISC) in Verilog. This project spanned 3 months and was debugged using ModelSim test benches, RTL gate level simulation and testing on a DE1-SOC FPGA.",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Car Loan Calculator",
+    subtitle: "Java and JFrame",
+    description:
+      "I Designed a car loan calculator GUI that calculates the monthly payment of a car loan given a down payment, interest, loan length and credit score.",
+    image: "./project-3.gif",
+    link: "https://github.com/Nicethanman/Car-Loan-Calculator",
+  },
+  {
+    title: "Personal Website",
+    subtitle: "React and TailwindCSS",
+    description:
+      "The website you're currently visiting was built using React, TailwindCSS and HTML. The site consists of a Google Maps Embedded API with a dynamic navigation bar.",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Java",
+  "React",
+  "Embedded C",
+  "Verilog",
+  "Linux",
+  "Microcontrollers",
+];
