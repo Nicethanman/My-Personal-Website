5af68bcc2a5f3cb948f5040bf78b9666f1e5b4f8
Nicholas Chang <143849035+Nicethanman@users.noreply.github.com>
2024-01-11 08:26:48 -0800

    updated project description

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -29,7 +29,7 @@ export default function About() {
               <img
                 className="object-cover object-center rounded"
                 alt="hero"
-                src="./coding.svg"
+                src="./pic_of_Nic.jpg"
               />
             </div>
           </div>
--- my-portfolio/src/data.js
+++ my-portfolio/src/data.js
@@ -3,24 +3,24 @@ export const projects = [
     title: "Hello Command",
     subtitle: "Firmware Development in C",
     description:
-      "I Implemented a command for UBC Orbit's ALEASAT satellite that output a Hello message. The command was tested the code by manually flashing to a Hercules microcontroller and writing a hardware in the loop test in Python",
-    image: "./project-1.gif",
+      "I Implemented a command for UBC Orbit's ALEASAT satellite that output a Hello message. The command was tested by manually flashing to a Hercules microcontroller and writing a hardware in the loop test in Python",
+    image: "./hello_command_pic.jpg",
     link: "https://github.com/Nicethanman/Hello-Command",
   },
   {
     title: "RISC Machine",
     subtitle: "System Verilog",
     description:
-      "In the class Computing Systems I, I created a Turing Complete reduced instruction set machine (RISC) in Verilog. This project spanned 3 months and was debugged using ModelSim test benches, RTL gate level simulation and testing on a DE1-SOC FPGA.",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+      "In the class Computing Systems I, I created a Turing Complete reduced instruction set machine (RISC) in Verilog. This project spanned 3 months and was debugged using ModelSim test benches, RTL gate level simulation and synthesis on a DE1-SOC FPGA.",
+    image: "./ModelSimPic.png",
+    link: "https://github.com/Nicethanman?tab=repositories",
   },
   {
     title: "Car Loan Calculator",
     subtitle: "Java and JFrame",
     description:
-      "I Designed a car loan calculator GUI that calculates the monthly payment of a car loan given a down payment, interest, loan length and credit score.",
-    image: "./project-3.gif",
+      "I designed a car loan calculator GUI that calculates the monthly payment of a car loan given a down payment, interest, loan length and credit score.",
+    image: "./car_loan_pic.png",
     link: "https://github.com/Nicethanman/Car-Loan-Calculator",
   },
   {
@@ -28,8 +28,8 @@ export const projects = [
     subtitle: "React and TailwindCSS",
     description:
       "This website was built using React, TailwindCSS and HTML. The site consists of a Google Maps Embedded API with a dynamic navigation bar.",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+    image: "./website_pic.png",
+    link: "https://github.com/Nicethanman/My-Personal-Website",
   },
 ];
 
